4a8e1fce466d8fefabca5aaf6f8a62f881747d7d
rhuanyago <rhuan.yago2014@live.com>
2021-06-20 00:10:45 -0300

    change01

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -13,7 +13,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        return view('welcome', ['nome' => 'Rhuan']);
+        return view('welcome', ['nome' => 'Rhuan', 'idade' => '22']);
     }
 
     /**
